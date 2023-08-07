8f1062f5601208865345b3167318ce9ff5aa30e6
meddlin <drushing.dev@gmail.com>
2023-08-07 18:20:30 -0500

    let's force the tests to pass and see if that helps

--- __tests__/pages/index.test.js
+++ __tests__/pages/index.test.js
@@ -5,6 +5,7 @@ import Home from '@/pages/index';
 describe('Home', () => {
   it('renders a heading', () => {
     render(<Home />);
+
     const text = screen.getByText('Get started by editing');
     expect(text).toBeInTheDocument();
   });
